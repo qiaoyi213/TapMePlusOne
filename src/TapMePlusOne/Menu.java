7703b2479c25e0a79e074465b4a8851b918900c5
vicky0619 <69741897+vicky0619@users.noreply.github.com>
2023-05-04 16:03:19 +0800

    menu add start and help

--- src/TapMePlusOne/Menu.java
+++ src/TapMePlusOne/Menu.java
@@ -1,5 +1,8 @@
 package TapMePlusOne;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.scene.control.Button;
 import javafx.scene.Scene;
 
@@ -8,11 +11,31 @@ public class Menu {
 	private Pane menuPane;
 	public Menu(){
 		menuPane = new Pane();
-		//Start button
-		Button startBtn = new Button("Start");
-		menuPane.getChildren().add(startBtn);
-		menuScene = new Scene(menuPane, 512, 720);
-	}
+		// Title
+        Text title = new Text("Tap +1");
+        title.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, 68));
+        title.setX(150);
+        title.setY(250);
+        menuPane.getChildren().add(title);
+        
+        // Start button
+        Button startBtn = new Button("Start");
+        startBtn.setPrefSize(100, 50);
+        startBtn.setLayoutX(200);
+        startBtn.setLayoutY(400);
+        startBtn.setStyle("-fx-background-color: purple; -fx-text-fill: white; -fx-font-size: 20pt;");
+        menuPane.getChildren().add(startBtn);
+        
+        // Help button
+        Button helpBtn = new Button("Help");
+        helpBtn.setPrefSize(100, 50);
+        helpBtn.setLayoutX(200);
+        helpBtn.setLayoutY(500);
+        helpBtn.setStyle("-fx-background-color: lightgray; -fx-text-fill: black; -fx-font-size: 20pt;");
+        menuPane.getChildren().add(helpBtn);
+        
+        menuScene = new Scene(menuPane, 512, 720);
+    }
 	public Scene getScene() {
 		return this.menuScene;
 	}
