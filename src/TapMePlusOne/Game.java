cbd08613ba6322e39e7ef17d543e35a1d96ee03a
qiaoyi213 <linqiaoyi213@gmail.com>
2023-05-05 04:29:25 +0800

    fix: Fix the animatino problem To-do: fix the sacn pad method and beautify

--- src/TapMePlusOne/Game.java
+++ src/TapMePlusOne/Game.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 import javafx.util.Duration;
 import javafx.util.Pair;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.animation.*;
@@ -28,7 +29,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 public class Game {
 	
-	private Button[][] pad;
+	private TButton[][] pad;
 	private boolean[][] vis;
 	private int score;
 	private Pane pane;
@@ -83,7 +84,7 @@ public class Game {
 	    pane.getChildren().add(aboveText);
 
 		
-		this.pad = new Button[7][7];		
+		this.pad = new TButton[7][7];		
 		this.score = 0;
 		for(int i=1;i<=5;i++) {
 			for(int j=0;j<=5;j++) {
@@ -93,16 +94,23 @@ public class Game {
 		
 		for(int i=1;i<=5;i++) {
 			for(int j=1;j<=5;j++) {
-				this.pad[i][j] = new Button(Integer.toString((int)(Math.random()*6)+1));
-				final Button nowBtn = this.pad[i][j];
+				this.pad[i][j] = new TButton();
+				this.pad[i][j].setText((Integer.toString((int)(Math.random()*6)+1)));
+				final TButton nowBtn = this.pad[i][j];
 				final int x = i;
 				final int y = j;
 				nowBtn.setPrefSize(100, 100);
 				
 				nowBtn.setStyle("-fx-background-color: #6666ff; -fx-border-radius: 50;");
+				nowBtn.setTextFill(Color.WHITE);
+				nowBtn.setPos(x,y);
 				nowBtn.setOnAction(event-> {
 					nowBtn.setText(Integer.toString(Integer.parseInt(nowBtn.getText())+1));
-					bfs(x,y);
+					disable_pad(true);
+				
+					bfs(nowBtn.getI(),nowBtn.getJ());
+					
+					
 				});
 				nowBtn.setTranslateX(30+110*(j-1));
 				nowBtn.setTranslateY(270+(i-1)*110);
@@ -119,12 +127,16 @@ public class Game {
 		return Integer.parseInt(btn.getText());
 	}
 	private void bfs(int x, int y) {
+		disable_pad(true);
 		Queue<Pair<Integer,Integer>> q = new LinkedList<>();
 		
 		q.add(new Pair<Integer, Integer>(x,y));
 		int target = getButtonNumber(this.pad[x][y]);
 		System.out.println(target);
-		showPad();
+		System.out.print(x);
+		System.out.print(" ");
+		System.out.println(y);
+		//showPad();
 		vis = new boolean[7][7];
 		e = new Direction[7][7];
 		while(q.size() > 0) {
@@ -171,7 +183,10 @@ public class Game {
 				}
 			}
 		}
-		if(counter < 3)return;
+		if(counter < 3) {
+			disable_pad(false);
+			return;
+		}
 		
 		for(int i=1;i<=5;i++) {
 			for(int j=1;j<=5;j++) {
@@ -282,7 +297,9 @@ public class Game {
 		
 		for(int i=5;i>=1;i--) {
 			for(int j=1;j<=5;j++) {
-				Button tmp = this.pad[i+b[i][j]][j];
+				this.pad[i+b[i][j]][j].setPos(this.pad[i][j].getI(),this.pad[i][j].getJ());
+				this.pad[i][j].setPos(this.pad[i][j].getI()+b[i][j], this.pad[i][j].getJ());
+				TButton tmp = this.pad[i+b[i][j]][j];
 				this.pad[i+b[i][j]][j] = this.pad[i][j];
 				this.pad[i][j] = tmp;
 			}	
@@ -292,18 +309,40 @@ public class Game {
 		for(int i=5;i>=1;i--) {
 			for(int j=1;j<=5;j++) {
 				if(getButtonNumber(this.pad[i][j]) == 0) {
-					this.pad[i][j].setText("1");
+					this.pad[i][j].setText(Integer.toString((int)(Math.random()*6+1)));
 					add_new_block(i,j);
-					Timeline t = new Timeline();
-					t.setCycleCount(10);
-					for(int k=i;k>0;k--) {						
-						t.getKeyFrames().add(move(this.pad[k][j],'D'));
+					for(int k=0;k<i;k++) {						
+						Timeline t = new Timeline();
+						t.setCycleCount(10);
+						t.getKeyFrames().add(move(this.pad[i][j],'D'));
+						st.getChildren().add(t);
 					}
-					st.getChildren().add(t);
+					
 				}
 			}
 		}
+		st.setOnFinished(e -> {
+			scan_pad();
+			disable_pad(false);
+		});
 		//showPad();
+		st.play();
+	}
+	private void scan_pad() {
+		SequentialTransition st = new SequentialTransition();
+		st.setDelay(Duration.millis(1000));
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5;j++) {
+				final int x = i, y = j;
+				
+				Timeline t = new Timeline();
+				t.getKeyFrames().add(new KeyFrame(Duration.millis(100), e -> {
+					disable_pad(true);
+					bfs(x,y);
+				}));
+				st.getChildren().add(t);
+			}
+		}
 		st.play();
 	}
 	private void add_new_block(int x,int y) {
@@ -311,11 +350,18 @@ public class Game {
 		this.pad[x][y].setTranslateY(160);
 		this.pad[x][y].setTranslateX(30+110*(y-1));
 		this.pad[x][y].setVisible(true);
-		
-		
 	}
-	private KeyFrame move(Button btn, char dir) {
-		
+	private void disable_pad(boolean flag) {
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5;j++) {
+				this.pad[i][j].setDisable(flag);
+			}
+		}
+	}
+	private KeyFrame move(TButton btn, char dir) {
+		if(btn == null) {
+			System.out.println("!!!NULL BTN");
+		}
 		KeyFrame kf = new KeyFrame(Duration.millis(30), event -> {
 			if(dir == 'U') {
 				btn.setTranslateY(btn.getTranslateY() - 11);
