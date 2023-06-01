37375256183e98c5de5f7a1ea06f71e0edcfd591
qiaoyi213 <linqiaoyi213@gmail.com>
2023-06-01 14:11:01 +0800

    style: Change style of scoreboard

--- src/TapMePlusOne/ScoreBoard.java
+++ src/TapMePlusOne/ScoreBoard.java
@@ -3,11 +3,15 @@ package TapMePlusOne;
 import java.util.ArrayList;
 
 import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.paint.Color;
@@ -20,38 +24,52 @@ public class ScoreBoard {
 	private Scene scene;
 	public ScoreBoard(Scene oldScene) throws Exception {
 		this.oldScene = oldScene;
-		Pane pane = new Pane();
-		pane.setStyle("-fx-background-image: url('file:resources/game_ver2.png'); -fx-background-size: contain;");
+
+		Group g = new Group();
+		ScrollPane scrollPane = new ScrollPane();
+		//pane.setStyle("-fx-background-image: url('file:resources/game_ver2.png'); -fx-background-size: contain;");
+		
 		Button exitBtn = new Button("");
 		ImageView imageView = new ImageView(new Image("file:resources/close.png"));
 		imageView.setFitWidth(50); 
         imageView.setPreserveRatio(true); 
         exitBtn.setGraphic(imageView);
         exitBtn.setStyle("-fx-background-color: transparent;");
-	    exitBtn.setAlignment(Pos.TOP_RIGHT);
+	    exitBtn.setLayoutX(0);
+	    exitBtn.setLayoutY(0);
 	    exitBtn.setOnAction(event -> {
 	    	Stage stage = (Stage)this.getScene().getWindow();
 	    	stage.setScene(oldScene);
 	    	stage.show();
 	    });
-	    pane.getChildren().add(exitBtn);
+	    
+	    g.getChildren().add(exitBtn);
+	    
 		ArrayList<Pair<String, Integer>> board = ScoreWrapper.getScoreBoard();
+		VBox vb = new VBox();
+		vb.setPrefWidth(600);
 		for(int i=0;i<board.size();i++) {
+			HBox hb = new HBox();
 			Text name = new Text(board.get(i).getKey());
-			name.setX(100);
-			name.setY(50*i+200);
-			name.setFill(Color.WHITE); // 設定文字顏色為白色
+			name.setX(50);
+			name.setY(50*i+100);
+			name.setFill(Color.BLACK); // 設定文字顏色為白色
 			name.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, 20)); // 設定字體和大小
 			Text score = new Text(board.get(i).getValue().toString());
-			score.setX(200);
-			score.setY(50*i+200);
-			score.setFill(Color.WHITE); // 設定文字顏色為白色
+			score.setX(100);
+			score.setY(50*i+100);
+			score.setFill(Color.BLACK); // 設定文字顏色為白色
 			score.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, 20)); // 設定字體和大小
-			pane.getChildren().add(name);
-			pane.getChildren().add(score);
+			hb.getChildren().add(score);
+			hb.getChildren().add(name);
+			
+			vb.getChildren().add(hb);
 		}
 		
-		scene = new Scene(pane, 600, 1024);
+		scrollPane.setContent(vb);
+		scrollPane.setLayoutY(50);
+		g.getChildren().add(scrollPane);
+		scene = new Scene(g, 600, 1024);
 	}
 	public Scene getScene() {
 		return this.scene;
