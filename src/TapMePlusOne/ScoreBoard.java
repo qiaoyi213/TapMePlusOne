9974edf818396cccce48244f9dbdbb0a1c3ec12f
vicky0619 <69741897+vicky0619@users.noreply.github.com>
2023-06-01 22:35:29 +0800

    update: scoreboard background

--- src/TapMePlusOne/ScoreBoard.java
+++ src/TapMePlusOne/ScoreBoard.java
@@ -11,6 +11,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -27,49 +28,55 @@ public class ScoreBoard {
 
 		Group g = new Group();
 		ScrollPane scrollPane = new ScrollPane();
-		//pane.setStyle("-fx-background-image: url('file:resources/game_ver2.png'); -fx-background-size: contain;");
-		
-		Button exitBtn = new Button("");
-		ImageView imageView = new ImageView(new Image("file:resources/close.png"));
-		imageView.setFitWidth(50); 
-        imageView.setPreserveRatio(true); 
+		scrollPane.setStyle("-fx-background: transparent; -fx-background-color: transparent;");
+
+        StackPane contentPane = new StackPane(); // 使用 StackPane 作為 ScrollPane 的內容
+
+        ImageView background = new ImageView(new Image("file:resources/game_ver2.png"));
+        background.setFitWidth(600);
+        background.setFitHeight(1024);
+
+        Button exitBtn = new Button("");
+        ImageView imageView = new ImageView(new Image("file:resources/close.png"));
+        imageView.setFitWidth(50);
+        imageView.setPreserveRatio(true);
         exitBtn.setGraphic(imageView);
         exitBtn.setStyle("-fx-background-color: transparent;");
-	    exitBtn.setLayoutX(0);
-	    exitBtn.setLayoutY(0);
-	    exitBtn.setOnAction(event -> {
-	    	Stage stage = (Stage)this.getScene().getWindow();
-	    	stage.setScene(oldScene);
-	    	stage.show();
-	    });
-	    
-	    g.getChildren().add(exitBtn);
-	    
-		ArrayList<Pair<String, Integer>> board = ScoreWrapper.getScoreBoard();
-		VBox vb = new VBox();
-		vb.setPrefWidth(600);
-		for(int i=0;i<board.size();i++) {
-			HBox hb = new HBox();
-			Text name = new Text(board.get(i).getKey());
-			name.setX(50);
-			name.setY(50*i+100);
-			name.setFill(Color.BLACK); // 設定文字顏色為白色
-			name.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, 20)); // 設定字體和大小
-			Text score = new Text(board.get(i).getValue().toString());
-			score.setX(100);
-			score.setY(50*i+100);
-			score.setFill(Color.BLACK); // 設定文字顏色為白色
-			score.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, 20)); // 設定字體和大小
-			hb.getChildren().add(score);
-			hb.getChildren().add(name);
-			
-			vb.getChildren().add(hb);
-		}
-		
-		scrollPane.setContent(vb);
-		scrollPane.setLayoutY(50);
-		g.getChildren().add(scrollPane);
-		scene = new Scene(g, 600, 1024);
+        exitBtn.setLayoutX(0);
+        exitBtn.setLayoutY(0);
+        exitBtn.setOnAction(event -> {
+            Stage stage = (Stage) scene.getWindow();
+            stage.setScene(oldScene);
+            stage.show();
+        });
+
+        ArrayList<Pair<String, Integer>> board = ScoreWrapper.getScoreBoard();
+        VBox vb = new VBox();
+        vb.setPrefWidth(600);
+        for (int i = 0; i < board.size(); i++) {
+            HBox hb = new HBox();
+            Text name = new Text(board.get(i).getKey());
+            name.setX(50);
+            name.setY(50 * i + 100);
+            name.setFill(Color.WHITE);
+            name.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, 20)); 
+            Text score = new Text(board.get(i).getValue().toString());
+            score.setX(100);
+            score.setY(50 * i + 100);
+            score.setFill(Color.WHITE); 
+            score.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, 20));
+            hb.getChildren().add(score);
+            hb.getChildren().add(name);
+
+            vb.getChildren().add(hb);
+        }
+
+        contentPane.getChildren().addAll(background, vb);
+        scrollPane.setContent(contentPane);
+        scrollPane.setLayoutY(0);
+        g.getChildren().addAll(scrollPane, exitBtn);
+
+        scene = new Scene(g, 600, 1024);
 	}
 	public Scene getScene() {
 		return this.scene;
